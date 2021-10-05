4101c383239ad99b6df20e14eb8e70bfc1454ffa
rdtrack <91665827+rdtrack@users.noreply.github.com>
2021-10-05 12:30:30 +0530

    Update Qu2_THREEBOX.cpp

--- Long_Challenge_Oct/Qu2_THREEBOX.cpp
+++ Long_Challenge_Oct/Qu2_THREEBOX.cpp
@@ -1,2 +1,30 @@
 // Question Link
 // https://www.codechef.com/OCT21C/status/THREEBOX
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int testc,num;
+    cin>>num;
+    for(testc=0;testc<num;testc++){
+        int a,b,c,d,sum,ans;
+        cin>>a>>b>>c>>d;
+        sum=a+b+c;
+        // cout<<sum<<endl;
+        if(sum<=d){
+            cout<<"1"<<endl;
+        }
+        else if(a+b<=d){
+            cout<<"2"<<endl;
+        }
+        else if(a+c<=d){
+            cout<<"2"<<endl;
+        }
+        else{
+           cout<<"3"<<endl;
+        }   
+    }
+    return 0;
+}
