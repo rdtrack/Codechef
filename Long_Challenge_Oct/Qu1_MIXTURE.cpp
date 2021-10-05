521a341bbafe4e3666907d3b3315ffbae9a1be4a
rdtrack <91665827+rdtrack@users.noreply.github.com>
2021-10-05 12:31:19 +0530

    Update Qu1_MIXTURE.cpp

--- Long_Challenge_Oct/Qu1_MIXTURE.cpp
+++ Long_Challenge_Oct/Qu1_MIXTURE.cpp
@@ -1,2 +1,25 @@
 // Question Link
 // https://www.codechef.com/OCT21C/problems/MIXTURE
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int testc,num;
+    cin>>num;
+    for(testc=0;testc<num;testc++){
+        int a,b;
+        cin>>a>>b;
+        if(a==0){
+            cout<<"Liquid"<<endl;
+        }
+        else if (b==0)
+        {
+            cout<<"Solid"<<endl;
+        }
+        else{
+            cout<<"Solution"<<endl;
+        }
+    }
+    return 0;
+}
